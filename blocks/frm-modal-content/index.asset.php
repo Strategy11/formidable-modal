3a225072550d19e3a9e262fcea95b129aa867f4a
Truong Giang <truongwp@gmail.com>
2023-05-10 16:49:58 +0700

    Fix position of insert block button

--- blocks/frm-modal-content/index.asset.php
+++ blocks/frm-modal-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '44b357ffe31ab49e3119');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'd452aa3b3cf3ce25a0fb');
