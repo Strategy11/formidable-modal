ea1549a6f15df0e685bb02c2c7c43e6ab75967c4
Truong Giang <truongwp@gmail.com>
2023-05-10 13:36:32 +0700

    Remove color settings

--- blocks/frm-modal-content/index.asset.php
+++ blocks/frm-modal-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '552116f128a19d97e8c0');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '8e5f617375bfe5c3cef8');
