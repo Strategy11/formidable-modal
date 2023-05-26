96ffb3fa019ee5b789fb6c2d5c16f8d8365b8ea4
Truong Giang <truongwp@gmail.com>
2023-05-26 22:48:53 +0700

    Clean output_modal() code

--- classes/frmBtsModApp.php
+++ classes/frmBtsModApp.php
@@ -265,37 +265,40 @@ class frmBtsModApp {
 	 */
 	public static function output_modal() {
 		global $frm_vars;
-		$allowed_sizes = array(
-			'small' => 'modal-sm',
-			'large' => 'modal-lg',
-		);
 
-		if ( isset( $frm_vars['modals'] ) && is_array( $frm_vars['modals'] ) ) {
-			foreach ( $frm_vars['modals'] as $i => $form_atts ) {
-				if ( ! empty( $form_atts['skip_modal_wrapper'] ) ) {
-					$modal = do_shortcode( $form_atts['mod_content'] );
-				} else {
-					$size = isset( $allowed_sizes[ $form_atts['size'] ] ) ? $allowed_sizes[ $form_atts['size'] ] : '';
-					$title = empty( $form_atts['modal_title'] ) ? $form_atts['label'] : $form_atts['modal_title'];
+		if ( ! isset( $frm_vars['modals'] ) || ! is_array( $frm_vars['modals'] ) ) {
+			return;
+		}
 
-					$modal = '<div id="frm-modal-' . esc_attr( $i ) . '"';
-					$modal .= ' class="modal fade ' . esc_attr( $form_atts['modal_class'] ) . '" tabindex="-1" role="dialog"';
-					$modal .= ' aria-labelledby="frmModalLabel-' . esc_attr( $i ) . '" aria-hidden="true">';
-					$modal .= '<div class="modal-dialog ' . esc_attr( $size ) . '">';
-					$modal .= '<div class="modal-content">';
-					$modal .= '<div class="modal-header">';
-					$modal .= '<h4 class="modal-title" id="frmModalLabel-' . esc_attr( $i ) . '">' . $title . '</h4>';
-					$modal .= '<a class="close frm_icon_font frm_cancel1_icon alignright" data-dismiss="modal" data-bs-dismiss="modal"></a>';
-					$modal .= '</div>';
-					$modal .= '<div class="modal-body">';
-					$modal .= do_shortcode( $form_atts['mod_content'] );
-					$modal .= '</div>';
-					$modal .= '</div>';
-					$modal .= '</div>';
-					$modal .= '</div>';
-				}
-				echo $modal; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+		foreach ( $frm_vars['modals'] as $i => $form_atts ) {
+			if ( ! empty( $form_atts['skip_modal_wrapper'] ) ) {
+				$modal = do_shortcode( $form_atts['mod_content'] );
+			} else {
+				$allowed_sizes = array(
+					'small' => 'modal-sm',
+					'large' => 'modal-lg',
+				);
+
+				$size = isset( $allowed_sizes[ $form_atts['size'] ] ) ? $allowed_sizes[ $form_atts['size'] ] : '';
+				$title = empty( $form_atts['modal_title'] ) ? $form_atts['label'] : $form_atts['modal_title'];
+
+				$modal = '<div id="frm-modal-' . esc_attr( $i ) . '"';
+				$modal .= ' class="modal fade ' . esc_attr( $form_atts['modal_class'] ) . '" tabindex="-1" role="dialog"';
+				$modal .= ' aria-labelledby="frmModalLabel-' . esc_attr( $i ) . '" aria-hidden="true">';
+				$modal .= '<div class="modal-dialog ' . esc_attr( $size ) . '">';
+				$modal .= '<div class="modal-content">';
+				$modal .= '<div class="modal-header">';
+				$modal .= '<h4 class="modal-title" id="frmModalLabel-' . esc_attr( $i ) . '">' . $title . '</h4>';
+				$modal .= '<a class="close frm_icon_font frm_cancel1_icon alignright" data-dismiss="modal" data-bs-dismiss="modal"></a>';
+				$modal .= '</div>';
+				$modal .= '<div class="modal-body">';
+				$modal .= do_shortcode( $form_atts['mod_content'] );
+				$modal .= '</div>';
+				$modal .= '</div>';
+				$modal .= '</div>';
+				$modal .= '</div>';
 			}
+			echo $modal; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 		}
 	}
 }
