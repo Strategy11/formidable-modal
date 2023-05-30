fa4f5db175d88223037909bb6908d678f518f218
Truong Giang <truongwp@gmail.com>
2023-05-30 21:33:15 +0700

    Change modal icon

--- blocks/frm-modal/index.asset.php
+++ blocks/frm-modal/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '69414b6395ace0b09568');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '7c225c29c011fb2d4981');
