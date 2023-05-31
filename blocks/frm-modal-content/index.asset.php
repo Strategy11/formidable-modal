907b2f30f4b66f98e4b8e7d39f4264e53cdc5bca
Truong Giang <truongwp@gmail.com>
2023-05-31 16:56:28 +0700

    Show modal overlay in the block editor

--- blocks/frm-modal-content/index.asset.php
+++ blocks/frm-modal-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'cee19becd0a4b69c0419');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'ef89e8ea7bf18f96d09e');
