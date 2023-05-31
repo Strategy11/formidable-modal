952963d9c34e7033287a464f85be0be7bdea3419
Truong Giang <truongwp@gmail.com>
2023-05-31 20:57:41 +0700

    Remove margin block start

--- blocks/frm-modal-content/index.asset.php
+++ blocks/frm-modal-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '0a23c5cba36e1cbd5674');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '37861f1184d7471cbc68');
