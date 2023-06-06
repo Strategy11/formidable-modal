bb4feeb8d441b8c4fd10142e4eaeff58b083106f
Truong Giang <truongwp@gmail.com>
2023-06-06 21:32:03 +0700

    Fix font size of close button

--- blocks/frm-modal-content/index.asset.php
+++ blocks/frm-modal-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '99b43ab1820f0352cb7c');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '338b771ed3f4207fecdf');
