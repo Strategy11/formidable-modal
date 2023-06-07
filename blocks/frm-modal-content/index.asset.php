a35703348f3bf228be9e092b21855ae550f9f99d
Truong Giang <truongwp@gmail.com>
2023-06-07 22:28:29 +0700

    Fix modal width in block editor

--- blocks/frm-modal-content/index.asset.php
+++ blocks/frm-modal-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '1940e97e1859a44d645f');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '8aa2f69a62c14e6fbddc');
