7342d3a54bd576041de362c294ca7bc8d26ae16d
Truong Giang <truongwp@gmail.com>
2023-06-07 20:09:11 +0700

    Align the close button

--- blocks/frm-modal-content/index.asset.php
+++ blocks/frm-modal-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'cd155dbdab0a7f2b9727');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'f250d5ea9f68491eb99b');
