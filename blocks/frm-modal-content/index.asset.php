7a10684c88625ef3fb4750420aaeb02c9bac6cd4
Truong Giang <truongwp@gmail.com>
2023-06-07 23:00:13 +0700

    Reduce modal header padding

--- blocks/frm-modal-content/index.asset.php
+++ blocks/frm-modal-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7f185b63ba5e8298f7d2');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '9b6f1f0ec05e21b5b295');
