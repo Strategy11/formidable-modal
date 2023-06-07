8d50ab3ea41ff8acc5abbef2405ff53aaab685b3
Truong Giang <truongwp@gmail.com>
2023-06-07 22:34:41 +0700

    Clean settings for modal content

--- blocks/frm-modal-content/index.asset.php
+++ blocks/frm-modal-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '8aa2f69a62c14e6fbddc');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '38e993674b3082560571');
