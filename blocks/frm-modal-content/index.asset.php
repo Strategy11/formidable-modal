9a72c8f5db5f3898a969463267b761e8510d7e8b
Truong Giang <truongwp@gmail.com>
2023-06-07 21:03:23 +0700

    Reduce modal body padding bottom

--- blocks/frm-modal-content/index.asset.php
+++ blocks/frm-modal-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'f250d5ea9f68491eb99b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '39aef71d8212db259161');
