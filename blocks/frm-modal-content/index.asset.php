c26faf72b62a1d4ef68f4cf5f9299cbfb957e41b
Truong Giang <truongwp@gmail.com>
2023-06-07 22:43:45 +0700

    Remove margin top of first and margin bottom of last block

--- blocks/frm-modal-content/index.asset.php
+++ blocks/frm-modal-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '38e993674b3082560571');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '7f185b63ba5e8298f7d2');
