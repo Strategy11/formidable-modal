95ec9f4dc53b308bce8105b44ea58babdf68059e
Truong Giang <truongwp@gmail.com>
2023-06-08 15:40:57 +0700

    Reduce modal title padding, reduce close icon size

--- blocks/frm-modal-content/index.asset.php
+++ blocks/frm-modal-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '9b6f1f0ec05e21b5b295');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'f07d3061e03b08b5a444');
