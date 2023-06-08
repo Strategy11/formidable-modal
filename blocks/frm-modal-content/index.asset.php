3910fcc44f1eb1066443e0bbe4b86165fe4ad890
Truong Giang <truongwp@gmail.com>
2023-06-08 15:51:11 +0700

    Fix close button doesn't work, change em to rem

--- blocks/frm-modal-content/index.asset.php
+++ blocks/frm-modal-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'f07d3061e03b08b5a444');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '0638b2b02159dcf61ef0');
