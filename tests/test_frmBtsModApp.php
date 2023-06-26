c4850097dcdb13c44226a4d7516b104b253931f7
Truong Giang <truongwp@gmail.com>
2023-06-26 16:18:46 +0700

    Fix phpunit

--- tests/test_frmBtsModApp.php
+++ tests/test_frmBtsModApp.php
@@ -13,11 +13,11 @@ class test_frmBtsModApp extends FrmUnitTest {
 		$this->assertTrue( 0 < strpos( $result, 'data-target="#' ) );
 
 		// Run through the add modal attrs method again.
-		$result = $this->run_private_method(
+		$result2 = $this->run_private_method(
 			array( 'frmBtsModApp', 'maybe_add_modal_attrs_to_button' ),
 			array( $button, $atts )
 		);
-		$this->assertEquals( $button, $result );
+		$this->assertEquals( $result2, $result );
 
 		// Another button with modal attrs.
 		$button = '<a href="#" data-toggle="modal" data-bs-toggle="modal" data-target="#target" data-bs-target="#target">Button</a>';
