2eb2b25483e6dc42c96cc0579409bd6fba585336
Truong Giang <truongwp@gmail.com>
2023-11-21 09:20:57 +0700

    Hide close button in the backend

--- blocks/frm-modal-content/index.asset.php
+++ blocks/frm-modal-content/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '9eb493a20d13aa5e106b');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '6bee14cd6e91f453350b');
